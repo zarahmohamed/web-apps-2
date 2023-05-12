f4decea29c4f3a6c1e8ab21f38d5b007b3847e87
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-05-12 21:01:38 +0000

    tacostagram

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,17 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  # def new
-  #   @post = Post.new
-  #   # render posts/new view with new Post form
-  # end
+  def new
+    @post = Post.new
+  end
 
-  # def create
-  #   # start with a new Post
-  #   @post = Post.new
-  #   # assign user-entered form data to Post's columns
-  #   # save Post row
-  #   # redirect user
-  # end
-  
+  def create
+    @post = Post.new
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["body"]
+    @post["imaage"] = params["post"]["imaage"]
+    @post.save
+    redirect_to "/posts"
+  end
+
 end
