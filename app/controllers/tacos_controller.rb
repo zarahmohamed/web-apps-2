3645ff5537cb17fc86bdf23037b0c00cd934bd3e
zarahmohamed <129216734+zarahmohamed@users.noreply.github.com>
2023-05-12 13:32:06 -0500

    Initial commit

---
+++ app/controllers/tacos_controller.rb
@@ -0,0 +1,7 @@
+class TacosController < ApplicationController
+
+  def index
+    # render tacos/index view
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources "tacos"
+  resources "dice"
+  resources "bitcoins"
+  resources "companies"
+  resources "contacts"
+  resources "posts"
+end
