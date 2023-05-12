9c87b3574c2fe86e0fd94b7d26e142b7e1165945
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-05-12 20:28:34 +0000

    3:28

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,22 +6,32 @@ class CompaniesController < ApplicationController
     # render companies/index view
   end
 
-  # def show
+  def show
   #   # find a Company
-      @company = Company.find_by({"id" => params["id"] })
+      @company = Company.find_by({ "id" => params["id"] })
   #   # render companies/show view with details about Company
-  # end
+  end
 
-  # def new
+  def new
   #   # render view with new Company form
-  # end
+  @company = Company.new
+  end
   
-  # def create
+  def create
   #   # start with a new Company
+  @company = Company.new
+
   #   # assign user-entered form data to Company's columns
+  @company["name"] = params["company"]["name"]
+  @company["city"] = params["company"]["city"]
+  @company["state"] = params["company"]["state"]
+
   #   # save Company row
+  @company.save 
+
   #   # redirect user
-  # end
+  redirect_to "/companies" 
+  end
 
   # def edit
   #   # find a Company
